4349a540ea7fc1f7e5b24240a6ad8de83c7e780f
1001101001 <vopare-addo@st.vvu.edu.gh>
2019-01-05 08:13:03 +0000

    new Js file added

---
+++ main.js
@@ -0,0 +1,9 @@
+var pwd = document.getElementById('pwd');
+var eye = document.getElementById('eye');
+
+eye.addEventListener('click', togglePass);
+///A function to show the password when you click on the eye Ican
+function togglePass(){
+  eye.classList.toggle('active');
+  (pwd.type == 'password') ? pwd.type = 'text' : pwd.type = 'password';
+}
